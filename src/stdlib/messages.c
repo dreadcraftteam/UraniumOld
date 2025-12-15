9c8ca27ba84f498765958b8527ab08be59955b7f
dreadcraftteam <dreadcraftteam@gmail.com>
2025-12-15 20:07:27 +0500

    Add stdlib
    library with standard stuff :)

---
+++ src/stdlib/messages.c
@@ -0,0 +1,42 @@
+//=============================================================//
+//
+// Messages to display in the terminal and in the console
+//
+// By DREADCRAFT
+//
+//=============================================================//
+
+#include "stdio.h"
+#include "stdarg.h"
+
+#include "messages.h"
+
+/* Standard message */
+void Message(const char* message, ...)
+{
+    va_list args;
+    va_start(args, message);
+    printf("");
+    vprintf(message, args);
+    va_end(args);
+}
+
+/* Error message */
+void Error(const char* message, ...)
+{
+    va_list args;
+    va_start(args, message);
+    printf("");
+    vprintf(message, args);
+    va_end(args);
+}
+
+/* Warning message */
+void Warning(const char* message, ...)
+{
+    va_list args;
+    va_start(args, message);
+    printf("");
+    vprintf(message, args);
+    va_end(args);
+}
---
+++ src/stdlib/messages.h
@@ -0,0 +1,24 @@
+//=============================================================//
+//
+// Messages to display in the terminal and in the console
+//
+// By DREADCRAFT
+//
+//=============================================================//
+
+#ifndef MESSAGES_H
+#define MESSAGES_H
+
+#include "stdio.h"
+#include "stdarg.h"
+
+/* Standard message */
+void Message(const char* message, ...);
+
+/* Error message */
+void Error(const char* message, ...);
+
+/* Warning message */
+void Warning(const char* message, ...);
+
+#endif // MESSAGES_H
